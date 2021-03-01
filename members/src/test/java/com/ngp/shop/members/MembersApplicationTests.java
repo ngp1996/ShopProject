1465f056898e729cb291a689d579434bd25253e9
Ngp <ngp1996@163.com>
2021-03-01 16:04:54 +0800

    项目初始化

---
+++ members/src/test/java/com/ngp/shop/members/MembersApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ngp.shop.members;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MembersApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
